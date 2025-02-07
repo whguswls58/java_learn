930f9017666128a059a14d498ae79bfcd56c54fd
user <user@DESKTOP-0BROA15>
2025-02-07 11:08:28 +0900

    Example of String Type conversion

---
+++ javaproject/src/p_250207/StirngConcatExample.java
@@ -0,0 +1,26 @@
+package p_250207;
+
+public class StirngConcatExample {
+
+	public static void main(String[] args) {
+
+		// 숫자 연산
+		int value = 10 + 2 + 8;
+		System.out.println("value: " + value);		// 20
+		
+		//문자열 결합 연산
+		String str1 = 10 + 2 + "8";
+		System.out.println("str1: " + str1);		//128
+		
+		String str2 = 10 + "2" + 8;
+		System.out.println("str2: " + str2);		//1028
+		
+		String str3 = "10" + 2 + 8;
+		System.out.println("str3: " + str3);		//1028
+		
+		String str4 = "10" + (2 + 8);				//1010
+		System.out.println("str4: " + str4);
+		
+	}
+
+}
