0fc5912e49ce7dc3e7b8a7422c408b96dfd5c253
user <user@DESKTOP-0BROA15>
2025-02-07 11:24:36 +0900

    why can not make pakage?

--- javaproject/src/p_250206/GitTest.java
+++ javaproject/src/p_250206/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("깃 커밋 테스트용");
+		System.out.println("왜 패키지파일이 안생길까?");
 	}
 
 }
