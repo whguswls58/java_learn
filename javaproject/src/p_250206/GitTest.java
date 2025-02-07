cdd5d63dfd2f60de88db3c3a9585c38d494251c8
user <user@DESKTOP-0BROA15>
2025-02-07 11:48:45 +0900

    resolve problem to push

--- javaproject/src/p_250206/GitTest.java
+++ javaproject/src/p_250206/GitTest.java
@@ -6,6 +6,10 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("깃 커밋 테스트용");
 		System.out.println("왜 패키지파일이 안생길까?");
+		// rejected non fast forward 오류
+		// master 변경사항을 pull 하지 않은 상태로 push하면 충돌남!!
+		// pull 해준후 push 시행
+		
 	}
 
 }
