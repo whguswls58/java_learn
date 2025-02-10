2f8b10898e27d6a5fd3035b68ad363b008f477a7
user <user@DESKTOP-0BROA15>
2025-02-10 14:14:01 +0900

    if문 예제

--- javaproject/src/p2025_02_10/If05.java
+++ javaproject/src/p2025_02_10/If05.java
@@ -7,8 +7,10 @@ public class If05 {
 		
 		System.out.println("E = " + Math.E);			// E값
 		System.out.println("PI = " + Math.PI);			// PI값
-		System.out.println("난수 = " + Math.random());	// 난수발생
+		System.out.println("난수 = " + Math.random());	// 난수발생 : 0.0 <= Math.random() < 1.0
 		
+		// 난수 발생 공식
+		// 난수 = ((int) (Math.random) * (상한값-하한값+1)) + 하한값;	// 난수 : 하한값 ~ 상한값
 		// 주사위 번호 뽑기 : 1 ~ 6
 		int num = (int) (Math.random() * 6) + 1;
 		System.out.println("num = " + num);
