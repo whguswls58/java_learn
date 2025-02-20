05834348f15bbbcee793a78985ceb284e717c2a0
hyeonjin <whguwls58@naver.com>
2025-02-20 10:21:06 +0900

    자바 과제 풀이

---
+++ javaproject/src/p2025_02_20/Lotto.java
@@ -0,0 +1,49 @@
+﻿package p2025_02_20;
+
+import java.util.Arrays;
+
+class Lotto {
+
+	public static void main(String[] args) {
+
+		int num[] = new int[6];
+
+		for (int i = 0; i < num.length; i++) {
+
+			num[i] = (int) (Math.random() * 45) + 1;
+//			num[0]=5, num[1]=5,
+			for (int j = 0; j < i; j++) {
+				if (num[i] == num[j]) {
+					i--;
+					break;
+				} 
+
+			} 
+		} 
+
+		
+		// 오름차순 정렬 : 버블정렬(자신의 옆자리에 있는 것과 비교)
+		int temp = 0;
+		for (int i = 0; i < num.length; i++){
+			for (int j = i+1; j < num.length; j++) {
+				if(num[i] > num[j]){
+					temp = num[i];
+					num[i] = num[j];
+					num[j] = temp;
+				}
+			}
+		}
+
+		// 생성자가 없는 클래스 -> 메소드가 전부 정적 메소드(객체 생성 불가)
+		Arrays.sort(num); // 오름차순 정렬
+
+		for (int k : num) {
+			System.out.print(k + "\t");
+		}
+	}
+}
+
+
+// 난수 발생 공식 
+// 난수 =(정수화)(Math.random()*(상한값-하한값+1)) + 하한값
+  
