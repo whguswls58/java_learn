37013f8b3593a6eea4d5f22cf4181116473dfa97
hyeonjin <whguwls58@naver.com>
2025-02-21 08:49:21 +0900

    2025.02.20. 자바과제

--- javaproject/src/p2025_02_20/CalendarTesk.java
+++ javaproject/src/p2025_02_20/CalendarTesk.java
@@ -8,9 +8,6 @@ public class CalendarTesk {
 		// TODO Auto-generated method stub
 
 		// Calendar 클래스 이용해서 오늘 날짜, 시간, 요일 출력
-		
-		String[] week = {"일", "월", "화", "수", "목", "금", "토"};
-		
 		Calendar c = Calendar.getInstance();
 		
 		// 오늘 날짜
@@ -23,20 +20,20 @@ public class CalendarTesk {
 		int mm = c.get(Calendar.MINUTE);
 		int s = c.get(Calendar.SECOND);
 		int ap = c.get(Calendar.AM_PM);
+		String amap;
 		
-//		if(ap == 0) {
-//			System.out.println("오전");
-//		}else {
-//			System.out.println("오후");
-//		}
+		if(ap == 0) {
+			amap = "오전";
+		}else {
+			amap = "오후";
+		}
 		
 		// 요일
 		int wk = c.get(Calendar.DAY_OF_WEEK)-1;
+		String[] week = {"일", "월", "화", "수", "목", "금", "토"};
 		
-		System.out.println(y + "-" + m + "-" + d + 
-							" / " + h + ":" + mm + ":" + s + " / " + week[wk] + "요일");
-		
-		
+		System.out.println(y + "년" + m + "월" + d + 
+							"일 " + amap + " "+ h + "시" + mm + "분" + s + "초 " + week[wk] + "요일");
 		
 	}
 
