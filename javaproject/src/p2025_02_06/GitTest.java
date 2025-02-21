8155c182f4fde6908e502966024bf84f12a0639c
whguswls58 <whguwls58@naver.com>
2025-02-21 09:47:34 +0900

    커밋테스트3

--- javaproject/src/p2025_02_06/GitTest.java
+++ javaproject/src/p2025_02_06/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
 		System.out.println("잔디가 잘 심겼으면 좋겠다.");
 		System.out.println("잔디가 잘 심겼으면 좋겠다.2");
 		System.out.println("커밋테스트");
+		System.out.println("커밋테스트2");
 		
 	}
 
