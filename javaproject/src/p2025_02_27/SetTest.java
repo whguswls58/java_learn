32d7139fc6756ffe4feaf91604b75bde2ecce17d
whguswls58 <whguswls58@naver.com>
2025-02-27 10:06:44 +0900

    TreeSet을 이용한 로또번호 생성

---
+++ javaproject/src/p2025_02_27/SetTest.java
@@ -0,0 +1,29 @@
+﻿package p2025_02_27;
+
+import java.util.*;
+
+public class SetTest {
+
+	public static void main(String[] args) {		
+		
+//		Set s = new HashSet();
+		TreeSet s = new TreeSet();		// 오름차순 정렬로 값 저장
+		
+		Random r = new Random();		
+		
+		int i = 0;			// 루프 횟수 카운팅
+		
+		while(true){
+//			int n = (int)(Math.random()*45) + 1; // 1 ~ 45 난수 발생
+			int n = r.nextInt(45)+1;			
+			s.add(n);
+			i++;
+			if(s.size() == 6){
+				System.out.println(s);
+				System.out.println(i + "번 루프 실행");
+				break;
+			}
+		}
+	}
+
+}
