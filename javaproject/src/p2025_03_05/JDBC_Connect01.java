998a92a0388a64161158b53b7c7deada3df4c616
whguswls58 <whguswls58@naver.com>
2025-03-05 16:55:57 +0900

    DB 환경구축 및 연동

---
+++ javaproject/src/p2025_03_05/JDBC_Connect01.java
@@ -0,0 +1,26 @@
+package p2025_03_05;
+
+import java.sql.*;
+
+public class JDBC_Connect01 {
+
+	public static void main(String[] args) {
+
+		/** ORACLE JDBC Driver Test ***************************/
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		/******************************************************/
+
+		/** My-SQL JDBC Driver Test **************************/
+//	String driver ="com.mysql.jdbc.Driver";
+		/*****************************************************/
+
+		try {
+			Class.forName(driver);		// JDBC Driver Loading
+			System.out.println("JDBC Driver Loading 성공~!!");
+
+		} catch (Exception e) {
+			System.out.println("JDBC Driver Loading 실패~!!");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ javaproject/src/sql/myoracle.sql
@@ -0,0 +1,7 @@
+--create table customer (no number(4) primary key,
+--					   						
+--
+--)
+
+select * from tab;
+select * from member01;
